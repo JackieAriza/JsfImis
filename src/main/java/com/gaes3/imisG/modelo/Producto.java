236121f881db7914e95090fde234a90a4f263844
Harold Daniel Morales <hdmorales082@misena.edu.co>
2022-11-23 00:07:00 -0500

    Se vuelven a generar getters, setters y se añade tostring de producto

--- src/main/java/com/gaes3/imisG/modelo/Producto.java
+++ src/main/java/com/gaes3/imisG/modelo/Producto.java
@@ -2,7 +2,6 @@ package com.gaes3.imisG.modelo;
 
 import java.util.Arrays;
 
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,11 +12,11 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="producto")
+@Table(name = "producto")
 public class Producto {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long idProductos;
 
 	private String nombreProducto;
@@ -28,80 +27,65 @@ public class Producto {
 
 	private long stock;
 
-	
-	//@OneToMany(mappedBy="producto",fetch=FetchType.LAZY)
-	//private List<Detalle_Por_Factura> detalle_por_factura;
-	
-	@ManyToOne 
-	@JoinColumn(name="fk_categoriaProductos")
-	private Categoriaproducto categoriaproducto;
+	// @OneToMany(mappedBy="producto",fetch=FetchType.LAZY)
+	// private List<Detalle_Por_Factura> detalle_por_factura;
 
+	@ManyToOne
+	@JoinColumn(name = "fk_categoriaProductos")
+	private Categoriaproducto categoriaproducto;
 
 	public long getIdProductos() {
 		return idProductos;
 	}
 
-
 	public void setIdProductos(long idProductos) {
 		this.idProductos = idProductos;
 	}
-
 
 	public String getNombreProducto() {
 		return nombreProducto;
 	}
 
-
 	public void setNombreProducto(String nombreProducto) {
 		this.nombreProducto = nombreProducto;
 	}
-
 
 	public double getValorProducto() {
 		return valorProducto;
 	}
 
-
 	public void setValorProducto(double valorProducto) {
 		this.valorProducto = valorProducto;
 	}
-
 
 	public String getEstadoProducto() {
 		return estadoProducto;
 	}
 
-
 	public void setEstadoProducto(String estadoProducto) {
 		this.estadoProducto = estadoProducto;
 	}
-
 
 	public long getStock() {
 		return stock;
 	}
 
-
 	public void setStock(long stock) {
 		this.stock = stock;
 	}
 
-
 	public Categoriaproducto getCategoriaproducto() {
 		return categoriaproducto;
 	}
-	
-	
-
 
 	@Override
 	public String toString() {
-		return  nombreProducto;
+		return "Producto [idProductos=" + idProductos + ", nombreProducto=" + nombreProducto + ", valorProducto="
+				+ valorProducto + ", estadoProducto=" + estadoProducto + ", stock=" + stock + ", categoriaproducto="
+				+ categoriaproducto + "]";
 	}
-
 
 	public void setCategoriaproducto(Categoriaproducto categoriaproducto) {
 		this.categoriaproducto = categoriaproducto;
 	}
 }
-	

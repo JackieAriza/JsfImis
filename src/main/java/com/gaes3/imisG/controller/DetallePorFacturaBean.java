bad118f922f0535ea0a2d5dd80a1888084065883
Harold Daniel Morales <hdmorales082@misena.edu.co>
2022-11-22 23:11:41 -0500

    Se Modifica el bean para el carrito de compras del modulo ventas

--- src/main/java/com/gaes3/imisG/controller/DetallePorFacturaBean.java
+++ src/main/java/com/gaes3/imisG/controller/DetallePorFacturaBean.java
@@ -2,21 +2,24 @@ package com.gaes3.imisG.controller;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletResponse;
+
+import org.primefaces.PrimeFaces;
 
 import com.gaes3.imisG.facadeImp.DetallePorFacturaDAO;
 import com.gaes3.imisG.facadeImp.FacturaDAO;
 import com.gaes3.imisG.facadeImp.ProductoDAO;
 import com.gaes3.imisG.modelo.Detalle_Por_Factura;
 import com.gaes3.imisG.modelo.Factura;
-import com.gaes3.imisG.modelo.Orden_de_compra;
 import com.gaes3.imisG.modelo.Producto;
 
 @ManagedBean(name = "detalleporfacturaBean")
@@ -29,10 +32,21 @@ public class DetallePorFacturaBean implements Serializable {
 	private Detalle_Por_Factura detalleporfactura;
 	private List<Factura> obtenerFactura;
 	private Factura factura;
-	private List<Producto> obtenerProductos;
+	private List<Producto> obtenerProductos = new ArrayList<>();
 	private Producto producto;
 	private List<Detalle_Por_Factura> detalleporfacturarp;
-	private Orden_de_compra ordendecompra;
+	
+	private int id;
+	
+	private ProductoDAO productoDao = new ProductoDAO();
+	
+	private static final ArrayList<Detalle_Por_Factura> listacarrito = new ArrayList<Detalle_Por_Factura>();
+
+	
+	
+	public ArrayList<Detalle_Por_Factura> getListacarrito() {
+		return listacarrito;
+	}
 
 	public Detalle_Por_Factura getDetalleporfactura() {
 		return detalleporfactura;
@@ -44,13 +58,19 @@ public class DetallePorFacturaBean implements Serializable {
 		return obtenerFactura;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public Factura getFactura() {
 		return factura;
 	}
 
 	public List<Producto> getObtenerProductos() {
-		ProductoDAO p = new ProductoDAO();
-		obtenerProductos = p.obtenerProducto();
 		return obtenerProductos;
 	}
 
@@ -83,11 +103,12 @@ public class DetallePorFacturaBean implements Serializable {
 		detalleporfactura = new Detalle_Por_Factura();
 		producto = new Producto();
 		factura = new Factura();
+		obtenerProductos = productoDao.obtenerProducto();
 	}
 
 	public String nuevo() {
 
-		return "/Ventas/nuevodetalle.xhtml";
+		return "/Ventas/nuevodetalle.xhtml?faces-redirect=true";
 	}
 	public String empleadodashnuevo() {
 
@@ -163,8 +184,166 @@ public class DetallePorFacturaBean implements Serializable {
 		d.eliminar(id);
 		System.out.println("Cliente Eliminado..");
 
-		return "/DashboardEmpelado/detalleporfactura.xhtml";
+		return "/DashboardEmpleado/detalleporfactura.xhtml";
 	}
 
+	
+	public void agregar() {
+		Detalle_Por_Factura df = new Detalle_Por_Factura();
+		Producto p = new Producto();
+		boolean verificar = false;
+		long cantidadtotal=0;
+		p = productoDao.buscar(producto.getIdProductos());
+		if(producto.getStock()>0) {			
+			if(p.getStock()>=producto.getStock()) {	
+				for(Detalle_Por_Factura detalle: listacarrito ) {
+					if(producto.getIdProductos() == detalle.getProducto().getIdProductos()) {
+						verificar = true;
+						cantidadtotal=cantidadtotal+detalle.getCantidadDetalle();
+					}
+				}
+				if(verificar==true) {
+					cantidadtotal=cantidadtotal+producto.getStock();
+				}
+				if(p.getStock()>=cantidadtotal) {
+					df.setIdDetalle(listacarrito.size() + 1);
+					df.setCantidadDetalle(producto.getStock());
+					df.setProducto(producto);
+					df.setSubTotal(producto.getValorProducto() * producto.getStock());
+					df.setValorUnitario(producto.getValorProducto());
+					df.setTotalGeneral(producto.getValorProducto()*producto.getStock());
+					listacarrito.add(df);
+					addMessage("Exitoso", "El producto fue agregado exitosamente");
+					df = new Detalle_Por_Factura();
+					producto = new Producto();
+					PrimeFaces.current().ajax().update("datosVenta");
+					PrimeFaces.current().ajax().update("detallesVenta:ventas");
+				}else {
+					addMessageError("Error", "La suma total de los productos es igual a " + cantidadtotal + " y no puede ser mayor a "+ p.getStock() + " que es la cantidad del inventario");
+				}
+			}else {
+				addMessageError("Error", "La cantidad no puede ser mayor a la que existe en el inventario");
+			}
+		}else {
+			addMessageError("Error", "La cantidad no puede ser menor o igual a 0");
+		}
+	}
 
+	public void encontrar() {
+		producto = productoDao.buscar(producto.getIdProductos());
+		if(producto!=null) {
+			addMessage("Exitoso","El producto " + producto.getNombreProducto() + " fue encontrado.");		
+		}else {
+			addMessageError("Error", "El produto no fue encontrado");
+		}
+	}
+	
+	public void eliminarDetalleCarrito(Detalle_Por_Factura detalle) {
+		listacarrito.remove(detalle);
+		ArrayList<Detalle_Por_Factura> lista = new ArrayList<>();
+		int id = 1;
+		for(Detalle_Por_Factura d: listacarrito){
+			d.setIdDetalle(id);
+			lista.add(d);
+			id++;
+		}
+		listacarrito.clear();
+		for(Detalle_Por_Factura d: lista) {
+			listacarrito.add(d);
+		}
+		PrimeFaces.current().ajax().update("datosVenta");
+		PrimeFaces.current().ajax().update("detallesVenta:ventas");
+	}
+	
+	public void editarDetalleCarrito(Detalle_Por_Factura detalle) {
+		producto =new Producto();
+		producto.setStock(detalle.getCantidadDetalle());
+		producto.setValorProducto(detalle.getValorUnitario());
+		producto.setIdProductos(detalle.getProducto().getIdProductos());
+		producto.setNombreProducto(detalle.getProducto().getNombreProducto());
+		id=(int) detalle.getIdDetalle();
+		System.out.println(id);
+		PrimeFaces.current().ajax().update("datosVenta2");
+		PrimeFaces.current().ajax().update("detallesVenta:ventas");
+		PrimeFaces.current().executeScript("PF('dlgl').show();");
+	}
+	
+	public void editarDetalle() {
+		Detalle_Por_Factura df = new Detalle_Por_Factura();
+		Producto p = new Producto();
+		boolean verificar = false;
+		long cantidadtotal=0;
+		p = productoDao.buscar(producto.getIdProductos());
+		if(producto.getStock()>0) {			
+			if(p.getStock()>=producto.getStock()) {	
+				for(Detalle_Por_Factura detalle: listacarrito ) {
+					if(producto.getIdProductos() == detalle.getProducto().getIdProductos()) {
+						verificar = true;
+						cantidadtotal=detalle.getCantidadDetalle();
+						break;
+					}
+				}
+				if(verificar==true) {
+					cantidadtotal=cantidadtotal+producto.getStock();
+				}
+				if(p.getStock()>=cantidadtotal) {	
+					System.out.println(producto);
+					df.setCantidadDetalle(producto.getStock());
+					df.setProducto(producto);
+					df.setSubTotal(producto.getValorProducto() * producto.getStock());
+					df.setValorUnitario(producto.getValorProducto());
+					df.setTotalGeneral(producto.getValorProducto()*producto.getStock());
+					System.out.println(id);
+					ArrayList<Detalle_Por_Factura> lista = new ArrayList<>();
+					for(Detalle_Por_Factura detalle: listacarrito) {
+						if(detalle.getIdDetalle()!=id) {
+							lista.add(detalle);
+							System.out.println("Prueba2");
+						}else {
+							lista.add(df);
+						}
+					}
+					listacarrito.clear();
+					int idlista=1;
+					for(Detalle_Por_Factura detalle: lista) {
+						detalle.setIdDetalle(idlista);
+						listacarrito.add(detalle);
+						idlista++;
+					}
+					addMessage("Exitoso", "El producto fue editado exitosamente");
+					df = new Detalle_Por_Factura();
+					producto = new Producto();
+					PrimeFaces.current().ajax().update("datosVenta");
+					PrimeFaces.current().ajax().update("detallesVenta:ventas");
+				}else {
+					addMessageError("Error", "La suma total de los productos es igual a " + cantidadtotal + " y no puede ser mayor a "+ p.getStock() + " que es la cantidad del inventario");
+				}
+			}else {
+				addMessageError("Error", "La cantidad no puede ser mayor a la que existe en el inventario");
+			}
+		}else {
+			addMessageError("Error", "La cantidad no puede ser menor o igual a 0");
+		}
+	}
+	
+	public void addMessageError(String summary, String detail) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, summary, detail);
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+
+	public void addMessage(String summary, String detail) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, detail);
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+
+	public void addMessageWarning(String summary, String detail) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_WARN, summary, detail);
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+	
+	public void limpiarcarrito() {
+		listacarrito.clear();
+		PrimeFaces.current().ajax().update("datosVenta");
+		PrimeFaces.current().ajax().update("detallesVenta:ventas");
+	}
 }
